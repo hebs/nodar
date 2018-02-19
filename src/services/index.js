04a2d67077d7c1a92bb1619cb826133f764a013c
Beau Shaw <mastermold@beaus-mbp.attlocal.net>
2018-02-19 13:56:43 -0600

    Generate authentication via the feathers CLI

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const smartNode = require('./smart-node/smart-node.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(smartNode);
+  app.configure(users);
 };
