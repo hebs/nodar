50bc2b841b273a568ecd2ec655fe4285e44502b1
Beau Shaw <mastermold@beaus-mbp.attlocal.net>
2018-02-19 14:39:38 -0600

    Create transactions service via CLI

---
+++ src/models/transactions.model.js
@@ -0,0 +1,15 @@
+// transactions-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const transactions = new Schema({
+    text: { type: String, required: true }
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('transactions', transactions);
+};
